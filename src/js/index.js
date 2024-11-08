c0f6a9a673c4439b1fb51efe27643255bfb8ca10
Bitbarlily <lilysvanoe@gmail.com>
2024-11-09 00:22:00 +0100

    fixed missing id symbol

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "a3f38o45c2eb0807db7cfft853e805ff";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  console.log(apiUrl);
+}
+
 function handleSearhSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#searchFormInput");
-  let cityElement = document.querySelector("mainCity");
+  let cityElement = document.querySelector("#mainCity");
   cityElement.innerHTML = searchInput.Value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#searchForm");
