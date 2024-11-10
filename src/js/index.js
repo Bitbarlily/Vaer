eaebc56ffd74d9e5dad3fec97c1b04921219cb87
Bitbarlily <lilysvanoe@gmail.com>
2024-11-10 21:23:10 +0100

    added id on temp and js function for temp

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,22 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#mainTemp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#mainCity");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
   let apiKey = "a3f38o45c2eb0807db7cfft853e805ff";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  console.log(apiUrl);
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearhSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#searchFormInput");
-  let cityElement = document.querySelector("#mainCity");
-  cityElement.innerHTML = searchInput.Value;
+
   searchCity(searchInput.value);
 }
 
